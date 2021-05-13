7331f6c96e5d4308a2263aefe6b27a37ecfde332
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-05-13 13:02:16 -0500

    added notes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+// importing components
 import CounterWithHooks from './components/CounterWithHooks'
 import TextDisplay from './components/TextDisplay'
 import ClassCounter from './components/ClassCounter'
 import EffectHook from './components/EffectHook'
 
-
+// main function to be exported
 export default function App() {
     return (
         <div>
             <h1>Hey from app</h1>
+            {/* calling components imported from above */}
             <ClassCounter />
             <CounterWithHooks />
             <TextDisplay />
--- src/components/ClassCounter.js
+++ src/components/ClassCounter.js
@@ -1,20 +1,26 @@
+// import necessary libraries - notice the green names glowing below
 import React, { Component } from 'react';
 
+//setup class based component
 class Counter extends Component {
+	//establish state
 	state = {
 		count: 0,
 	};
 
+	//function to modify state
 	setCount = () => {
 		this.setState({ count: this.state.count + 1 });
 	};
 
+	//function to render output to user
 	render() {
 		return (
 			<div>
+				{/* display initial state and state changes */}
 				<h2>This is a counter using a class</h2>
 				<h1>{this.state.count}</h1>
-
+				{/* button that calls function to increment count */}
 				<button onClick={this.setCount}>Click to Increment</button>
 			</div>
 		);
