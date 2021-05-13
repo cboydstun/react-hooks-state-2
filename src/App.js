b747b41046d81d1d8e825b07e9170875526b3fdc
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-05-13 10:11:31 -0500

    added TextDisplay component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import CounterWithHooks from './components/CounterWithHooks'
+import TextDisplay from './components/TextDisplay'
 
 export default function App() {
     return (
         <div>
             <h1>Hey from app</h1>
             <CounterWithHooks />
+            <TextDisplay />
         </div>
     )
 }
---
+++ src/components/TextDisplay.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function TextDisplay() {
+    const [text, setText] = React.useState('text here');
+    const updateText = (event) => {
+        setText(event.target.value)
+    }
+    return (
+        <div>
+            <h1>Show Text: {text}</h1>
+            <input type="text" onChange={updateText} />
+        </div>
+    );
+}
+
+export default TextDisplay;
